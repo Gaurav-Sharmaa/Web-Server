b00d47fd020e441720844ed84e4122790784316c
Gaurav Sharma <xogaurav01ox@gmail.com>
2025-04-29 16:22:55 +0530

    updated 404 page

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,22 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
+    let (status_line, filename) = 
     if buffer.starts_with(get) {
-        let contents = fs::read_to_string("index.html").unwrap();
-
-        let response = format!(
-            // construct a valid response
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        ("HTTP/1.1 200 OK", "index.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    
+    let contents = fs::read_to_string(filename).unwrap();
 
-        let response = format!(
-            "{}\r\nContent-Lenght: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
+    let response = format!(
+        "{}\r\nContent-Lenght: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
